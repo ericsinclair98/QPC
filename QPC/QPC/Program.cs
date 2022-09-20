9f926db7c15632096ecfbab675874e21c1f58f1d
Eric Sinclair <59191988+ericsinclair98@users.noreply.github.com>
2022-09-20 13:20:43 +1000

    Banner Media Queries Start

--- QPC/QPC/Program.cs
+++ QPC/QPC/Program.cs
@@ -8,7 +8,6 @@ using QPC.Areas.Identity;
 using QPC.Data;
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
